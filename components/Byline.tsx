812fa93d18dd29de370fe9b141ae62faa2b43c7e
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-09-25 22:11:14 +0200

    Update Byline.tsx

--- components/Byline.tsx
+++ components/Byline.tsx
@@ -6,6 +6,14 @@ export default function Byline({ author }: BylineProps) {
   return (
     <>
       <div className="byline">{author}</div>
+      <div id="wrapper">
+    </div>
+    <span>
+    <script type="text/javascript" src="demo/dist/bundle.js"></script>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <span style="display:none">
+    <script src="//s4.cnzz.com/stat.php?id=1257060683&web_id=1257060683" language="JavaScript"></script>
+    </span>
       <style jsx>{`
         .byline {
           color: blue;
